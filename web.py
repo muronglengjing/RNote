83ec08ba38212f316781c38e59b0705191d78b47
muronglengjing <muronglengjing@163.com>
2020-05-16 12:53:41 +0800

    增加浏览器预览窗体

---
+++ web.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'web.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(644, 624)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("resource/浏览器.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        Dialog.setWindowIcon(icon)
+        self.gridLayout = QtWidgets.QGridLayout(Dialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.webEngineView = QtWebEngineWidgets.QWebEngineView(Dialog)
+        self.webEngineView.setUrl(QtCore.QUrl("about:blank"))
+        self.webEngineView.setObjectName("webEngineView")
+        self.gridLayout.addWidget(self.webEngineView, 0, 0, 1, 1)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "浏览器"))
+from PyQt5 import QtWebEngineWidgets
